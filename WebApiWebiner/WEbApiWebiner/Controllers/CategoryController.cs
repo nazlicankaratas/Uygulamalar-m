7ff2baa6be8a1d832adc08a110dbb728148715bc
Your Name <nazlicann.karatas@gmail.com>
2019-07-27 20:11:23 +0300

    added webapiwebiner app

---
+++ WebApiWebiner/WEbApiWebiner/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEbApiWebiner.Models.Dto;
+using WEbApiWebiner.Models.Orm;
+
+namespace WEbApiWebiner.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        NorthwindEntities db = new NorthwindEntities();
+
+        public List<CategoryModel> GetAllCategories()
+        {
+            List<CategoryModel> catList = db.Categories.Select(x => new CategoryModel()
+            {
+                KategoriAdi=x.CategoryName,
+                Aciklamasi=x.Description
+            }).ToList();
+            return catList;
+        }
+        [HttpGet]
+        public CategoryModel CategoryByID(int id)
+        {
+            Categories cat = db.Categories.Find(id);
+            CategoryModel catModel = new CategoryModel();
+            catModel.Aciklamasi = cat.Description;
+            catModel.KategoriAdi = cat.CategoryName;
+
+            return catModel;
+        }
+    }
+}
---
+++ WebApiWebiner/WEbApiWebiner/Models/Dto/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEbApiWebiner.Models.Dto
+{
+    public class CategoryModel
+    {
+        public string KategoriAdi { get; set; }
+
+        public string Aciklamasi { get; set; }
+    }
+}
